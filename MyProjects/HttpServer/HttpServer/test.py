3f545150d3dc4978a31c963950249cf91095d638
haoen110 <haoenwei@outlook.com>
2019-11-21 20:12:27 +0800

    1

--- MyProjects/HttpServer/HttpServer/test.py
+++
@@ -1,4 +0,0 @@
-from socket import *
-
-s = socket()
-s.connect(('127.0.0.1', 8000))
--- MyProjects/database/tb.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE sdsc5003 CHARACTER SET utf8;
-
-CREATE TABLE users(id INT(11) AUTO_INCREMENT PRIMARY KEY,
-                   username VARCHAR(30),
-                   email VARCHAR(100),
-                   password VARCHAR(100),
-                   register_date TIMESTAMP  DEFAULT CURRENT_TIMESTAMP);
-
